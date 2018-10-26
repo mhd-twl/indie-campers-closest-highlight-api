e89d5b36f22bb235a329b66779e1cba8588f4722
mhd-twl <mhmdaltawel@gmail.com>
2018-10-26 23:17:48 +0200

    Modify new API

--- main/app/Http/Controllers/RoutePathController.php
+++ main/app/Http/Controllers/RoutePathController.php
@@ -172,12 +172,12 @@ class RoutePathController extends Controller
     }
     public function show_geo_diff($lat1 , $long1, $lat2 , $long2)
     {
-        $p1 = new Point($lat1 , $long1) ; $p2 = new Point($lat2 , $long2);
-        $routepath = new RoutePath($p1, $p2);
-        $point1 = $this->find_point_label($lat1 , $long1);
-        $point2 = $this->find_point_label($lat2 , $long2);
         if(is_numeric($lat1)&& is_numeric($long1)&& is_numeric($lat2)&& is_numeric($long2))
         {
+            $p1 = new Point($lat1 , $long1) ; $p2 = new Point($lat2 , $long2);
+            $routepath = new RoutePath($p1, $p2);
+            $point1 = $this->find_point_label($lat1 , $long1);
+            $point2 = $this->find_point_label($lat2 , $long2);
                 if($point1== $point2) return $this->apiResponse([],"Start is the end point!" ,200);  
                 if(isset($this->graph[$point1]) && isset($this->graph[$point2]) ) {
                     $result= [
